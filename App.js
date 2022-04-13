628b83373d9f767175558b0c84f3f39d8ebe7509
Rola <carlosrolandiaz@gmail.com>
2022-04-13 09:58:18 +0200

    coomit

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Modal } from 'react-native';
+import MapView from 'react-native-maps';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    backgroundColor: '#fff',
+    borderRadius: 4,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height:3,
+    }
+  },
+  map: {
+    height : Dimensions.get('window').height,
+    width: Dimensions.get.('window').width,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -18,3 +30,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map}/>
+      <Modal 
+        animationType='slide'
+        transparent= {true}
+        visible={true}
+      >
+        <View style={styles.center}>
+          <View style={styles.modalView}>
+            <Text></Text>
+          </View>        
+        </View>
+      </Modal>      
+    </View>
+  )
+}
+
+
